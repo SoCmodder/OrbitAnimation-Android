cfdf096d06219af8fca5a03ab6874a7744978ae8
Mitch Miller <socmodder@gmail.com>
2019-04-05 18:11:54 -0400

    initial

---
+++ app/src/main/java/world/mitchmiller/orbitanimation/MainActivity.kt
@@ -0,0 +1,16 @@
+package world.mitchmiller.orbitanimation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun doStuff() {
+
+    }
+}
---
+++ app/src/main/java/world/mitchmiller/orbitanimation/example/CircleConstraintsActivity.java
@@ -0,0 +1,60 @@
+package world.mitchmiller.orbitanimation.example;
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import world.mitchmiller.orbitanimation.R;
+
+import java.util.concurrent.TimeUnit;
+
+public class CircleConstraintsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        ImageView mercuryImage = findViewById(R.id.mercury_imageview);
+        ImageView venusImage = findViewById(R.id.venus_imageview);
+        ImageView earthImage = findViewById(R.id.earth_imageview);
+        ImageView marsImage = findViewById(R.id.mars_image);
+        ImageView saturnImage = findViewById(R.id.saturn_image);
+        ImageView neptuneImage = findViewById(R.id.neptune_imageview);
+
+        // Them 'nacci numbers, yo
+        ValueAnimator mercuryAnimator = animatePlanet(mercuryImage, TimeUnit.SECONDS.toMillis(1));
+        ValueAnimator venusAnimator = animatePlanet(venusImage, TimeUnit.SECONDS.toMillis(2));
+        ValueAnimator earthAnimator = animatePlanet(earthImage, TimeUnit.SECONDS.toMillis(3));
+        ValueAnimator marsAnimator = animatePlanet(marsImage, TimeUnit.SECONDS.toMillis(5));
+        ValueAnimator saturnAnimator = animatePlanet(saturnImage, TimeUnit.SECONDS.toMillis(8));
+        ValueAnimator neptuneAnimator = animatePlanet(neptuneImage, TimeUnit.SECONDS.toMillis(13));
+
+        mercuryAnimator.start();
+        venusAnimator.start();
+        earthAnimator.start();
+        marsAnimator.start();
+        saturnAnimator.start();
+        neptuneAnimator.start();
+    }
+
+    private ValueAnimator animatePlanet(ImageView planet, long orbitDuration) {
+        ValueAnimator anim = ValueAnimator.ofInt(0, 359);
+        anim.addUpdateListener(valueAnimator -> {
+            int val = (Integer) valueAnimator.getAnimatedValue();
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) planet.getLayoutParams();
+            layoutParams.circleAngle = val;
+            planet.setLayoutParams(layoutParams);
+        });
+        anim.setDuration(orbitDuration);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setRepeatMode(ValueAnimator.RESTART);
+        anim.setRepeatCount(ValueAnimator.INFINITE);
+
+        return anim;
+    }
+
+}
