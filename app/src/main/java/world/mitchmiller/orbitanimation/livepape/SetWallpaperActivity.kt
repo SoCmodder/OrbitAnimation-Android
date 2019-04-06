a60c6cc0cf7afb144abb323dbbe267a77a037b71
Mitch Miller <socmodder@gmail.com>
2019-04-06 11:58:53 -0400

    set up initial wallpaper code

---
+++ app/src/main/java/world/mitchmiller/orbitanimation/livepape/SetWallpaperActivity.kt
@@ -0,0 +1,25 @@
+package world.mitchmiller.orbitanimation.livepape
+
+import android.app.WallpaperManager
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import world.mitchmiller.orbitanimation.R
+import world.mitchmiller.orbitanimation.livepape.service.DerpOrbitWallpaperService
+
+class SetWallpaperActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_wallpaper)
+    }
+
+    public fun onClick(view: View) {
+        var intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+            ComponentName(this@SetWallpaperActivity, DerpOrbitWallpaperService::class.java))
+        startActivity(intent)
+    }
+}
